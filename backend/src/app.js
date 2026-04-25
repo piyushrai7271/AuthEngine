bee8ef1bcfe61af85379683081795680da791cd0
piyush rai <piyushrai7271@gmail.com>
2026-04-25 17:17:07 +0530

    adding basic setup of front end

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,20 @@
 import express from "express";
+import cors from "cors";
+import corsOptions from "./configs/cors.js";
 import cookieParser from "cookie-parser";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+// USE SHARED CORS CONFIG
+app.use(cors(corsOptions));
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// ✅ routes (example)
+//  routes (example)
 import userRoutes from "./routes/auth.routes.js";
 app.use("/api/auth", userRoutes);
 
---
+++ fronend/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="bg-slate-900 text-white min-h-screen">
+       <h1 className='font-extrabold'> Welcome Home </h1>
+    </div>
+  )
+}
+
+export default App
